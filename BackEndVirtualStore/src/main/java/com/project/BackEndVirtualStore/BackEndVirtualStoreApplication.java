6838ed6fad44683b044ee545c5bf3554dc493628
Everton <everton6943@gmail.com>
2023-11-16 09:10:57 -0300

    first commit

---
+++ BackEndVirtualStore/src/main/java/com/project/BackEndVirtualStore/BackEndVirtualStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.BackEndVirtualStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndVirtualStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndVirtualStoreApplication.class, args);
+	}
+
+}
---
+++ BackEndVirtualStore/src/test/java/com/project/BackEndVirtualStore/BackEndVirtualStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BackEndVirtualStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndVirtualStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
